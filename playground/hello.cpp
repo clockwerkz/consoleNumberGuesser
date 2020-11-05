8f866198aa5905b82276722780647acc5f969589
Carlos <clockwerkz@gmail.com>
2020-11-04 18:50:39 -0800

    example of vector pointers arrow operator

--- playground/hello.cpp
+++ playground/hello.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
+
 using std::cout;
+
+void PrintVectorSize(std::vector<int> *v)
+{
+    cout<< "Size of vector: " << v->size() << std::endl;
+}
 
 int main()
 {
-    cout << "Hello world! \n";
+    std::vector<int> v1 {1,2,3,4,5}; //size of 5
+    std::vector<int> *v2 = new std::vector<int>{4,5,6}; //initial size of 3
+    v2->push_back(10); //Increasing size to 4
+    cout << "Printing sizes of Vectors: \n";
+    PrintVectorSize(&v1);
+    PrintVectorSize(v2);
 }
